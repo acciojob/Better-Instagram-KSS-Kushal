f765a5b7c339fab44933be6163a1781d7d10e48a
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-18 21:28:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+let start;
+let end;
+function drag(ev) {
+	start = ev.target;
+}
+
+function drop(ev) {
+  ev.preventDefault();
+	end = ev.target;
+	swapEle(start, end);
+}
+
+function swapEle(start, end) {
+	// const temp = start;
+	start.replaceWith(end, start);
+	// end.replaceWith(temp);
+}
