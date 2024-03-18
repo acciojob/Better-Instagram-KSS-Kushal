f1d4b386a220f703056d7e5b3264d9bc81203043
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-18 23:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-//your code here
-function allowDrop(ev) {
-  ev.preventDefault();
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-
-let start;
-let end;
-function drag(ev) {
-	start = ev.target;
+ 
+function allowDrop(e) {
+  e.preventDefault();
 }
-
-function drop(ev) {
-  ev.preventDefault();
-	end = ev.target;
-	swapEle(start, end);
-}
-
-function swapEle(start, end) {
- // Create a temporary storage for the start element
-    let temp = document.createElement("div");
-    temp.appendChild(start.cloneNode(true));
-
-    // Replace start with end
-    start.parentNode.replaceChild(end.cloneNode(true), start);
-
-    // Replace end with temp (original start)
-    end.parentNode.replaceChild(temp.firstChild, end);
-}
-
-it('should have 6 draggable elements', () => {
-  cy.visit('http://localhost:3000') // replace with your page's URL
-  cy.wait(5000) // wait for 5 seconds
-  for (let index = 1; index <= 6; index++) {
-    cy.get(`#div${index}`).should('have.length', 1)
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
   }
-})
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
+}
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
